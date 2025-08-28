0bbd8f74999df628e4d0498b582625c31b9b3ff4
Mitesh Sharma <mitsharm@redhat.com>
2025-08-28 17:25:02 +0530

    update

--- setup-automation/setup-vscode.sh
+++ setup-automation/setup-vscode.sh
@@ -9,6 +9,7 @@ echo "%rhel ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/rhel_sudoers
 chmod 440 /etc/sudoers.d/rhel_sudoers
 sudo -u rhel mkdir -p /home/rhel/.ssh
 sudo -u rhel chmod 700 /home/rhel/.ssh
+sudo -u rhel rm -rf /home/rhel/.ssh/id_rsa*
 sudo -u rhel ssh-keygen -t rsa -b 4096 -C "rhel@$(hostname)" -f /home/rhel/.ssh/id_rsa -N ""
 sudo -u rhel chmod 600 /home/rhel/.ssh/id_rsa*
 
